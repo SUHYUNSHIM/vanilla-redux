834dcff647c762b819d390d7cf134724e827a8b0
SUHYUNSHIM <lisa9963@naver.com>
2023-12-04 22:26:51 +0900

    subscribe
    -

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {createStore} from "redux"
 const add = document.getElementById("add");
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
-const countModifier = (count =0, action) => {
+number.innerText = 0;
+const countModifier = (count = 0, action) => {
     if(action.type === "ADD"){
         return count +1;
     }else if(action.type === "MINUS"){
@@ -14,7 +15,19 @@ const countModifier = (count =0, action) => {
 
 };
 const countStore = createStore(countModifier)
-countStore.dispatch({type : "ADD"});
-countStore.dispatch({type : "ADD"});
-countStore.dispatch({type : "MINUS"});
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange)
+const handleAdd = () => {
+    countStore.dispatch({type : "ADD"})
+}
+const handleMinus = () => {
+    countStore.dispatch({type : "MINUS"})
+}
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click",handleMinus)
+//add.addEventListener("click", () => countStore.dispatch({type : "ADD"}))
 
