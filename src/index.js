e2ec8552a603fc3a91e83979ab8ccc5e345dd461
SUHYUNSHIM <lisa9963@naver.com>
2023-12-16 22:10:25 +0900

    vanilla redux
    -

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 number.innerText = 0;
-const countModifier = (count = 0, action) => {
-    if(action.type === "ADD"){
-        return count +1;
-    }else if(action.type === "MINUS"){
-        return count -1;
-    }else{
-        return count;
-    }
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+    switch(action.type){
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count -1;
+        default:
+            return count;
+    };
 };
 const countStore = createStore(countModifier)
 
